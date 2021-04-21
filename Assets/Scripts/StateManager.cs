c281bfa4deba90cf497ae838b8287c8895aff130
Faizi <rockstar.mr5@gmail.com>
2021-04-21 16:08:22 +0530

    OpenWeatherTweaked

--- Assets/Scripts/StateManager.cs
+++ Assets/Scripts/StateManager.cs
@@ -1,117 +1,189 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
-public class StateManager : MonoBehaviour {
-	public WeatherData weatherData;
-	public string currentWeather;
-	private bool rain;
-	private bool snow;
-	private bool cloudy;
-	private bool sunny;
-	public GameObject rainObject;
-	public GameObject snowObject;
-	public GameObject cloudyObject;
-	public GameObject sunnyObject;
-	void Update () {
-		currentWeather = weatherData.Info.currently.icon;
+public class StateManager : MonoBehaviour
+{
+    public WeatherData weatherData;
+    public string currentWeather;
+    private bool rain;
+    private bool snow;
+    private bool cloudy;
+    private bool sunny;
+    public GameObject rainObject;
+    public GameObject snowObject;
+    public GameObject cloudyObject;
+    public GameObject sunnyObject;
+	List<string> rainie = new List<string>();
+	List<string> snowie = new List<string>();
+	List<string> cloudie = new List<string>();
+	List<string> sunnie = new List<string>();
 
-		if (currentWeather == "rain") {
-			SpawnRain ();
-		} else if (currentWeather == "snow" || currentWeather == "sleet") {
-			SpawnSnow ();
-		} else if (currentWeather == "cloudy" || currentWeather == "partly-cloudy-day" || currentWeather == "partly-cloudy-night" || currentWeather == "fog") {
-			SpawnCloudy ();
-		} else if (currentWeather == "clear-day" || currentWeather == "clear-night" || currentWeather == "wind") {
-			SpawnSunny ();
-		} else {
-			None ();
-		}
-	}
 
-	void SpawnRain () {
-		rain = true;
-		rainObject.SetActive (true);
-		if (snow) {
-			StartCoroutine (DisableSnow());
-		} else if (cloudy) {
-			StartCoroutine (DisableCloudy());
-		} else if (sunny) {
-			StartCoroutine (DisableSunny());
-		}
-	}
-	void SpawnSnow () {
-		snow = true;
-		snowObject.SetActive (true);
-		if (rain) {
-			StartCoroutine (DisableRain());
-		} else if (cloudy) {
-			StartCoroutine (DisableCloudy());
-		} else if (sunny) {
-			StartCoroutine (DisableSunny());
-		}
-	}
-	void SpawnCloudy () {
-		cloudy = true;
-		cloudyObject.SetActive (true);
-		if (snow) {
-			StartCoroutine (DisableSnow());
-		} else if (rain) {
-			StartCoroutine (DisableRain());
-		} else if (sunny) {
-			StartCoroutine (DisableSunny());
-		}
-	}
-	void SpawnSunny () {
-		sunny = true;
-		sunnyObject.SetActive (true);
-		if (snow) {
-			StartCoroutine (DisableSnow());
-		} else if (cloudy) {
-			StartCoroutine (DisableCloudy());
-		} else if (rain) {
-			StartCoroutine (DisableRain());
-		}
-	}
-	void None () {
-		if (rain) {
-			StartCoroutine (DisableRain());
-		} else if (cloudy) {
-			StartCoroutine (DisableCloudy());
-		} else if (sunny) {
-			StartCoroutine (DisableSunny());
-		} else if (snow) {
-			StartCoroutine (DisableSnow());
-		}
-	}
+    void Start()
+    {
+        string[] rainy_list = { "Rain", "Drizzle", "Thunderstorm" };
+        string[] snow_list = { "Snow", "Sleet" };
+        string[] cloud_list = { "Clouds", "Mist", "Smoke", "Dust", "Haze", "Fog", "Sand", "Dust", "Ash", "Sqail", "Tornado" };
+        string[] sunny_list = { "Clear" };
 
-	IEnumerator DisableRain() {
-		rain = false;
-		rainObject.GetComponent<ParticleSystem> ().Stop();
-		rainObject.GetComponent<Animator> ().Play ("rain_exit");
-		yield return new WaitForSeconds(5);
-		rainObject.SetActive (false);
-	}
+        rainie.AddRange(rainy_list);
+        snowie.AddRange(snow_list);
+        cloudie.AddRange(cloud_list);
+        sunnie.AddRange(sunny_list);
+    }
+    void Update()
+    {
+		try{
+        	currentWeather = weatherData.Info.weather[0].main;
+		} catch(Exception e) {
+            currentWeather = "none";
+        }
+		
+        Debug.Log("Weather: " + currentWeather);
 
-	IEnumerator DisableSnow() {
-		snow = false;
-		snowObject.GetComponent<ParticleSystem> ().Stop();
-		snowObject.GetComponent<Animator> ().Play ("snow_exit");
-		yield return new WaitForSeconds(5);
-		snowObject.SetActive (false);
-	}
+        if (rainie.Contains(currentWeather))
+        {
+            SpawnRain();
+        }
+        else if (snowie.Contains(currentWeather))
+        {
+            SpawnSnow();
+        }
+        else if (cloudie.Contains(currentWeather))
+        {
+            SpawnCloudy();
+        }
+        else if (sunnie.Contains(currentWeather))
+        {
+            SpawnSunny();
+        }
+        else
+        {
+            None();
+        }
+    }
 
-	IEnumerator DisableCloudy() {
-		cloudy = false;
-		cloudyObject.GetComponent<Animator> ().Play ("cloudy_exit");
-		yield return new WaitForSeconds(5);
-		cloudyObject.SetActive (false);
-	}
+    void SpawnRain()
+    {
+        rain = true;
+        rainObject.SetActive(true);
+        if (snow)
+        {
+            StartCoroutine(DisableSnow());
+        }
+        else if (cloudy)
+        {
+            StartCoroutine(DisableCloudy());
+        }
+        else if (sunny)
+        {
+            StartCoroutine(DisableSunny());
+        }
+    }
+    void SpawnSnow()
+    {
+        snow = true;
+        snowObject.SetActive(true);
+        if (rain)
+        {
+            StartCoroutine(DisableRain());
+        }
+        else if (cloudy)
+        {
+            StartCoroutine(DisableCloudy());
+        }
+        else if (sunny)
+        {
+            StartCoroutine(DisableSunny());
+        }
+    }
+    void SpawnCloudy()
+    {
+        cloudy = true;
+        cloudyObject.SetActive(true);
+        if (snow)
+        {
+            StartCoroutine(DisableSnow());
+        }
+        else if (rain)
+        {
+            StartCoroutine(DisableRain());
+        }
+        else if (sunny)
+        {
+            StartCoroutine(DisableSunny());
+        }
+    }
+    void SpawnSunny()
+    {
+        sunny = true;
+        sunnyObject.SetActive(true);
+        if (snow)
+        {
+            StartCoroutine(DisableSnow());
+        }
+        else if (cloudy)
+        {
+            StartCoroutine(DisableCloudy());
+        }
+        else if (rain)
+        {
+            StartCoroutine(DisableRain());
+        }
+    }
+    void None()
+    {
+        if (rain)
+        {
+            StartCoroutine(DisableRain());
+        }
+        else if (cloudy)
+        {
+            StartCoroutine(DisableCloudy());
+        }
+        else if (sunny)
+        {
+            StartCoroutine(DisableSunny());
+        }
+        else if (snow)
+        {
+            StartCoroutine(DisableSnow());
+        }
+    }
 
-	IEnumerator DisableSunny() {
-		sunny = false;
-		sunnyObject.GetComponent<Animator> ().Play ("sunny_exit");
-		yield return new WaitForSeconds(5);
-		sunnyObject.SetActive (false);
-	}
+    IEnumerator DisableRain()
+    {
+        rain = false;
+        rainObject.GetComponent<ParticleSystem>().Stop();
+        rainObject.GetComponent<Animator>().Play("rain_exit");
+        yield return new WaitForSeconds(5);
+        rainObject.SetActive(false);
+    }
+
+    IEnumerator DisableSnow()
+    {
+        snow = false;
+        snowObject.GetComponent<ParticleSystem>().Stop();
+        snowObject.GetComponent<Animator>().Play("snow_exit");
+        yield return new WaitForSeconds(5);
+        snowObject.SetActive(false);
+    }
+
+    IEnumerator DisableCloudy()
+    {
+        cloudy = false;
+        cloudyObject.GetComponent<Animator>().Play("cloudy_exit");
+        yield return new WaitForSeconds(5);
+        cloudyObject.SetActive(false);
+    }
+
+    IEnumerator DisableSunny()
+    {
+        sunny = false;
+        sunnyObject.GetComponent<Animator>().Play("sunny_exit");
+        yield return new WaitForSeconds(5);
+        sunnyObject.SetActive(false);
+    }
 }
--- Assets/Scripts/WeatherData.cs
+++ Assets/Scripts/WeatherData.cs
@@ -3,11 +3,14 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.Networking;
 using UnityEngine.UI;
+using System.Collections.Generic;
+using System.Linq;
 
 public class WeatherData : MonoBehaviour {
 	private float timer;
 	public float minutesBetweenUpdate;
-	public WeatherInfo Info;
+	// public WeatherInfo Info;
+	public OpenWeather Info;
 	public string API_key;
 	private float latitude;
 	private float longitude;
@@ -20,6 +23,7 @@ public class WeatherData : MonoBehaviour {
 		longitude = getLocation.longitude;
 		locationInitialized = true;
 	}
+
 	void Update() {
 		if (locationInitialized) {
 			if (timer <= 0) {
@@ -30,28 +34,27 @@ public class WeatherData : MonoBehaviour {
 			}
 		}
 	}
-	private IEnumerator GetWeatherInfo()
-	{
-		var www = new UnityWebRequest("https://api.darksky.net/forecast/" + API_key + "/" + latitude + "," + longitude)
-		{
+
+	private IEnumerator GetWeatherInfo(){
+		var www = new UnityWebRequest("https://api.openweathermap.org/data/2.5/weather?id=1791121&appid=a5243c4f8aac51d476f00e8c7ae2c5d1"){
 			downloadHandler = new DownloadHandlerBuffer()
 		};
 
 		yield return www.SendWebRequest();
 
-		if (www.isNetworkError || www.isHttpError)
-		{
-			//error
+		if (www.isNetworkError || www.isHttpError){
+			Debug.Log("error");
+			//error 
 			yield break;
 		}
 
-		Info = JsonUtility.FromJson<WeatherInfo>(www.downloadHandler.text);
-		currentWeatherText.text = "Current weather: " + Info.currently.summary;
+		Info = JsonUtility.FromJson<OpenWeather>(www.downloadHandler.text);
+		// currentWeatherText.text = "Current weather: " + Info.currently.summary;
 	}
 }
+
 [Serializable]
-public class WeatherInfo
-{
+public class WeatherInfo{
 	public float latitude;
 	public float longitude;
 	public string timezone;
@@ -60,8 +63,7 @@ public class WeatherInfo
 }
 
 [Serializable]
-public class Currently
-{
+public class Currently{
 	public int time;
 	public string summary;
 	public string icon;
@@ -82,3 +84,72 @@ public class Currently
 	public double visibility;
 	public double ozone;
 }
+
+[Serializable]
+public class OpenWeather{
+	public Coord coord;
+	public List<Weather> weather = new List<Weather>();
+	// public string base;
+	public Wind wind;
+	public Main main;
+	public Clouds clouds;
+	public Sys sys;	
+	public int timezone;
+	public int id;
+	public string name;
+	public int cod;
+	public int visibility;
+	public int dt;
+
+	public string weatherType(){
+        Debug.Log(weather.Count);
+        return weather.ElementAt(0).main;
+    }
+}
+
+[Serializable]
+public class Coord{
+	public float lon;
+	public float lat;
+}
+
+[Serializable]
+public class Weather{
+    public int id;
+    public string main;
+    public string description;
+    public string icon;
+	
+}
+
+[Serializable]
+public class Main{
+	public float temp;
+    public float feels_like;
+	public float temp_min;
+	public float temp_max;
+	public float pressure;
+	public float humidity;
+	
+}
+
+[Serializable]
+public class Wind{
+	public float speed;
+	public float deg;
+}
+
+[Serializable]
+public class Clouds{
+	public int all;
+}
+
+[Serializable]
+public class Sys{
+	public int type;
+	public int id;
+	public string country;
+	public int sunrise;
+	public int sunset;	
+}
+
